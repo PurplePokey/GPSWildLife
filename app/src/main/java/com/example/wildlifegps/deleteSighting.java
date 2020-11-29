0687086199269e8988f14dd741fe12bf051d71ec
priya <priyagillan52@gmail.com>
2020-11-29 15:55:53 -0600

    updated delete sighting method

--- app/src/main/java/com/example/wildlifegps/deleteSighting.java
+++ app/src/main/java/com/example/wildlifegps/deleteSighting.java
@@ -7,8 +7,15 @@ import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
 
 public class deleteSighting extends Activity {
+
+    private final deleteSighting activity = deleteSighting.this;
 
     Button b;
     DBHandler dbh;
@@ -19,25 +26,28 @@ public class deleteSighting extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sighting_view);
-        b=(Button)findViewById(R.id.delete_sighting);
-        final Context context = getApplicationContext();
+
+        b = (Button)findViewById(R.id.delete_sighting);
 
         try
         {
-            dbh = new DBHandler(context);
+            dbh = new DBHandler(activity);
+
         }
         catch(SQLiteException e)
         {
             System.out.print("ERROR");
         }
+
         b.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 dbh.deleteSighting(sighting);
 
-                Intent i = new Intent(context, Sighting.class);
-                startActivity(i);
+                Toast.makeText(activity, "Sighting Successfully Deleted", Toast.LENGTH_LONG).show();
+                Intent intentDelete = new Intent(getApplicationContext(), ListView.class);
+                startActivity(intentDelete);
             }
         });
     }
