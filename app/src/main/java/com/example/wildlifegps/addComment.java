67a926c66ce15927932cf165048d3118587b82d9
priya <priyagillan52@gmail.com>
2020-11-29 18:38:22 -0600

    added comment logic

---
+++ app/src/main/java/com/example/wildlifegps/addComment.java
@@ -0,0 +1,65 @@
+package com.example.wildlifegps;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteException;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class addComment extends AppCompatActivity {
+
+    private final addComment activity = addComment.this;
+
+    Button b;
+    TextInputEditText box;
+    DBHandler dbh;
+    Sighting sighting;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sighting_view);
+
+        b = (Button)findViewById(R.id.post_comment);
+        box = (TextInputEditText)findViewById(R.id.comment_textbox);
+
+        try
+        {
+            dbh = new DBHandler(activity);
+
+        }
+        catch(SQLiteException e)
+        {
+            System.out.print("ERROR");
+        }
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if(!box.getText().toString().matches(""))
+                {
+                    // not null not empty
+                    final String content = box.getText().toString();
+                    Comment comment = new Comment();
+                    comment.setComment(content);
+                    dbh.addComment(comment);
+                    Toast.makeText(activity, "Comment added!", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    //null or empty
+                    Toast.makeText(activity, "Enter text to make a comment", Toast.LENGTH_LONG).show();
+                }
+
+                Intent intentAddComment = new Intent(getApplicationContext(), Sighting.class);
+                startActivity(intentAddComment);
+            }
+        });
+    }
+}
